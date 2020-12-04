57d96dfdffec3ae7c56a244fcddbd93a67ebf10e
jp-loran <loran@pop-os.localdomain>
2020-12-04 14:49:34 -0600

    Primera pagina en github

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.slider').slider({
+    	indicators: false,
+    	interval: 2000,
+    	duration: 2000
+    });
+    $('.tabs').tabs({
+    	duration: 3000
+    });
+     $('.collapsible').collapsible();
+     $('input.autocomplete').autocomplete({
+      data: {
+        "Diseño Web": null,
+        "Redes de Datos": null,
+      }
+    });
+ }); 
